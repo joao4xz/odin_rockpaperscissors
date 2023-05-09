09d8a8fb17b33ec5b69140b63cec0d496e6a3ad6
joao4xz <joao.4xz@gmail.com>
2023-05-09 11:56:46 -0300

    Add Html interaction and Style.

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ function translateComputerChoice(num){
   }
 }
 
-function getPlayerChoice(){
-  return prompt("Rock, Paper or Scissors?");
-}
-
 function playRound(playerSelection, computerSelection) {
   if(playerSelection === "rock" && computerSelection === "scissors"){
     playerScore++;
@@ -50,7 +46,7 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game(playerScore, computerScore){
+function score(playerScore, computerScore){
   if(playerScore > computerScore){
     return `You won with ${playerScore} points. Against ${computerScore} points.`;
   }
@@ -64,17 +60,72 @@ function game(playerScore, computerScore){
 
 let playerScore = 0;
 let computerScore = 0;
-
-for(let i = 0; i < 5; i++){
+let round = 0
+let divFlag = 0;
+function game(playerSelection){
   let computerSelection = getComputerChoice();
-  let playerSelection = getPlayerChoice().toLowerCase();
   
   computerSelection = translateComputerChoice(computerSelection);
 
-  console.log(`You chose ${playerSelection}`);
-  console.log(`The computer chose ${computerSelection}`);
+  container = document.getElementById('container');
+  if(round != 0){
+    container.removeChild(document.getElementById('remove'));
+    container.removeChild(document.getElementById('remove2'));
+    container.removeChild(document.getElementById('remove3'));
+  }
+  if(divFlag === 1){
+    container.removeChild(document.getElementById('remove4'));
+    divFlag = 0;
+  }
+  
+  div = document.createElement('div');
+  div2 = document.createElement('div');
+  div3 = document.createElement('div');
 
-  console.log(playRound(playerSelection, computerSelection));
+
+  div.appendChild(document.createTextNode(`You chose ${playerSelection}`));
+  div.appendChild(document.createElement('br'));
+  div.appendChild(document.createElement('br'));
+  div.appendChild(document.createTextNode(`The computer chose ${computerSelection}`));
+  div.setAttribute('class', 'Text');
+  div.setAttribute('id', 'remove');
+  container.appendChild(div);
+
+  div2.appendChild(document.createTextNode(playRound(playerSelection, computerSelection)));
+  div2.setAttribute('class', 'Text');
+  div2.setAttribute('id', 'remove2');
+  container.appendChild(div2);
+
+  div3.appendChild(document.createTextNode(`Your score: ${playerScore}`));
+  div3.appendChild(document.createElement('br'));
+  div3.appendChild(document.createElement('br'));
+  div3.appendChild(document.createTextNode(`The computer score: ${computerScore}`));
+  div3.setAttribute('class', 'Text');
+  div3.setAttribute('id', 'remove3');
+  container.appendChild(div3);
+
+  round++;
+
+  if(playerScore === 5 || computerScore === 5){
+    div4 = document.createElement('div');
+    div4.appendChild(document.createTextNode(score(playerScore, computerScore)));
+    div4.setAttribute('class', 'Text');
+    div4.setAttribute('id', 'remove4');
+    container.removeChild(document.getElementById('remove'));
+    container.removeChild(document.getElementById('remove2'));
+    container.removeChild(document.getElementById('remove3'));
+    container.appendChild(div4);
+    playerScore = 0;
+    computerScore = 0;
+    round = 0
+    divFlag = 1;
+  }
 }
 
-console.log(game(playerScore, computerScore));
+buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    game(button.className)
+  });
+});
